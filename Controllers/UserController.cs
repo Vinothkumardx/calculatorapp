2a6b80341f950d0bfb5b512bffbbade57990e973
Vinothkumardx <vinothkumardx98@gmail.com>
2024-03-21 18:02:14 +0530

    login and registration added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register(RegisterRequest model)
+        {
+            return View(model);
+        }
+
+        public IActionResult Login (LoginRequest login)
+        {
+           
+                return View(login);
+            
+           
+        }
+    }
+}
---
+++ wwwroot/js/RegisterScript.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("myapp", []);
+app.controller("myctrl", function ($scope, $http) {
+    $scope.Registeradd = function () {
+
+        var username = $scope.username;
+        var password = $scope.password;
+        var email = $scope.email;
+        var firstname = $scope.firstname;
+        var lastname = $scope.lastname;
+
+        var requestdata = {
+            username :username,
+            password : password,
+            Email : email,
+            FirstName: firstname,
+            LastName: lastname
+
+        };
+        
+        $http.post('https://localhost:7028/api/User/register', requestdata)
+            .then(function (response) {
+               
+                alert('Data Register successfully:', response);
+            })
+            .catch(function (error) {
+               
+                alert("Fill the all field...!");
+            });
+
+    }
+})
